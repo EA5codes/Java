f19b945d7521278f8b99b475bc41356a1becc4b3
EsA <ea.avc@outlook.com>
2024-01-23 22:51:48 +0100

    first

---
+++ Example/AdviceTemperature.java
@@ -0,0 +1,28 @@
+// Projekt: give advice according to the temperature.
+// Event according to the weather degree 
+
+import java.util.Scanner;
+
+public class AdviceTemperature {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Degree : ");
+		int temperature = scan.nextInt();
+		String event = " ";
+		
+		if ( temperature < 5 ) { 
+			event = "Skiing";
+		}else if ( temperature < 15) {
+			event = "Theater";
+		}else if ( temperature < 25) {
+			event = "Grilling";
+		}else {
+			event = "Swimming";
+		}
+		
+		System.out.println("Adviced event : " + event);
+	}
+	
+
+}
+
